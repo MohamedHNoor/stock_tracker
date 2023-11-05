1aaebe53f1eef2412660eb475461488e9ee00de0
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-05 23:42:50 +0200

    Add front-end stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,7 @@
 class UserStocksController < ApplicationController
+
+ def create
+  stock = Stock.new_lookup(params[:ticker])
+  @user_stock = UserStock.create(user: current_user, stock: stock)
+ end
 end
