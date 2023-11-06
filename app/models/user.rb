f1fabb03e34d3b97f761f3c6cc691d71d2170565
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-06 20:43:14 +0200

    Add new attributes to signup and edit forms and add strong parameters to applicationController

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  validates :first_name, :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
