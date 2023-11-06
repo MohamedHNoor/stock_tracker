d85cc75b07cf2067f671f7bfc6813a83d5d5783e
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-06 15:31:41 +0200

    Add functionality to remove tracking stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,6 +9,13 @@ class UserStocksController < ApplicationController
   @user_stock = UserStock.create(user: current_user, stock: stock)
   flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
   redirect_to portfolio_path
- 
+ end
+
+ def destroy
+  stock = Stock.find(params[:id])
+  user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+  user_stock.destroy
+  flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+  redirect_to portfolio_path
  end
 end
