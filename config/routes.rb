c09a9a7d4dd183f0517fbd1a7e69937b8d559bf3
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-10 00:12:06 +0200

    Remove tracking functionality and Add friend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'friends', to: 'users#friends'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
 end
