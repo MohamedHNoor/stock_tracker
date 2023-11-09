e5c4bc899df1e7fb3a1917f078728b4a901b2af5
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-10 01:17:16 +0200

    Upgraded rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_06_185020) do
+ActiveRecord::Schema.define(version: 2023_11_09_231615) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
