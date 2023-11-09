7fbf43bed1e490fcd7e95a99c3f1d33fba89756f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-09 23:43:01 +0200

    Add search method and display search result on the browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,25 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search
-    render json: params[:friend]
+  def search  
+  if params[:friend].present?
+   @friends = User.search(params[:friend])
+   @friends = current_user.except_current_user(@friends)
+   if @friends
+    respond_to do |format|
+     format.js { render partial: 'users/friend_result'}
+    end
+   else
+    respond_to do |format|
+     flash.now[:alert] = "Couldn't find user"
+     format.js { render partial: 'users/friend_result'}
+    end
+   end
+  else 
+   respond_to do |format|
+     flash.now[:alert] = "Please enter a friend name or email to search"
+     format.js { render partial: 'users/friend_result'}
+    end
+  end
   end
 end
